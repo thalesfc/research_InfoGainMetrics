c8c6f2488e8cb92bbd8344dcb7e94a5511128f06
Geraldo Franciscani <gfrancis@Wilson.local>
2013-10-09 16:57:26 -0300

    Fix base_teste.py

--- base_1/base_teste.py
+++ base_1/base_teste.py
@@ -12,8 +12,8 @@ PRINT = 32
 
 logging.basicConfig(filename="stat_base_teste.log", fileMODE="w", level=logging.INFO, format="[ %(asctime)s ] %(levelname)s : %(message)s")
 
-urls = ["http://www.informatik.uni-trier.de/~ley/db/conf/infovis/index.html"]
-names = ["IEEE Symposium on Information Visualization (INFOVIS)"]
+urls = ["http://www.informatik.uni-trier.de/~ley/db/conf/infovis/index.html","http://www.informatik.uni-trier.de/~ley/db/conf/ieeevast/index.html","http://www.informatik.uni-trier.de/~ley/db/conf/vissym/index.html","http://www.informatik.uni-trier.de/~ley/db/conf/iv/index.html","http://www.informatik.uni-trier.de/~ley/db/conf/apvis/index.html","http://www.informatik.uni-trier.de/~ley/db/conf/IEEEcgiv/index.html"]
+names = ["IEEE Symposium on Information Visualization (INFOVIS)","IEEE Conference on Visual Analytics Science and Technology","EuroVis / Joint Eurographics - IEEE TCVG Symposium on Visualization","International Conference on Information Visualisation","Asia Pacific Symposium on Information Visualisation","Computer Graphics, Imaging and Vision"]
 
 count = 0
 if MODE == 'PAPERS':
@@ -31,19 +31,21 @@ for i_url, url in enumerate(urls):
         papers = open("files/cnf_{}/edition_{}/papers.txt".format(i_url, i_edition))
         logging.info("[Conference {}][Edition {}][# Papers] {}".format(i_url, i_edition, sum(1 for line in open("files/cnf_{}/edition_{}/papers.txt".format(i_url, i_edition)))))
         for i_pap, pap in enumerate(papers):
-            if os.path.exists("files/cnf_{}/edition_{}/pdf_{}.txt".format(i_url, i_edition, i_pap)) and os.path.exists("files/cnf_{}/edition_{}/keywords_{}.txt".format(i_url, i_edition, i_pap)):
+            if os.path.exists("files/cnf_{}/edition_{}/paper_{}.txt".format(i_url, i_edition, i_pap)) and os.path.exists("files/cnf_{}/edition_{}/keywords_{}.txt".format(i_url, i_edition, i_pap)):
+                if not len(open("files/cnf_{}/edition_{}/keywords_{}.txt".format(i_url, i_edition, i_pap)).read()) == 0:
+                    logging.info("[Conference {}][Edition {}][Paper-Anchor {}] {}".format(i_url,i_edition, i_pap, pap.rstrip()))
 
-                if MODE == 'PAPERS':
-                    super_file = open("files/cnf_{}/edition_{}/pdf_{}.txt".format(i_url, i_edition, i_pap)).read()
-                elif MODE == 'KEYWORDS':
-                    super_file = open("files/cnf_{}/edition_{}/keywords_{}.txt".format(i_url, i_edition, i_pap)).read()
+                    if MODE == 'PAPERS':
+                        super_file = open("files/cnf_{}/edition_{}/paper_{}.txt".format(i_url, i_edition, i_pap)).read()
+                    elif MODE == 'KEYWORDS':
+                        super_file = open("files/cnf_{}/edition_{}/keywords_{}.txt".format(i_url, i_edition, i_pap)).read()
 
-                ffile = super_file.replace('\n', ' ')
-                print >> f, ffile
-                count += 1
-                if count == PRINT:
-                        print "##############", count
-                        print super_file
-                #logging.info("[Conference {}][Edition {}][Paper-Anchor {}] {}".format(i_url,i_edition, i_pap, pap.rstrip()))
+                    ffile = super_file.replace('\n', ' ')
+                    print >> f, ffile
+                    count += 1
+                    if count == PRINT:
+                            print "##############", count
+                            print super_file
+                    #logging.info("[Conference {}][Edition {}][Paper-Anchor {}] {}".format(i_url,i_edition, i_pap, pap.rstrip()))
 
 print >> sys.stderr, "# of pdfs", count
